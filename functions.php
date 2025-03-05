74cc4307a9c6b114fb82ca183acbf893d819f981
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-05 16:30:47 +0100

    filter function +  need fix to error

--- functions.php
+++ functions.php
@@ -1,33 +1,33 @@
 <?php
 
-function randomPasswordGenerator($length, $alphabetic = true, $numbers = true, $symbols = true) {
+function randomPasswordGenerator($length, $alphabetic = "on", $numbers = "on", $symbols = "on") {
 
     $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $allNumbers = '0123456789';
     $allSymbols = '!@#$%^&*()-_=+';
-
-
-if(!$alphabetic){
-    $alphabet= '';
-}
-
-if(!$numbers){
-    $allNumbers= '';
-}
-
-if(!$numbers){
-    $allSymbols= '';
-}
-
-    $caratteri = $alphabet . $allNumbers . $allSymbols;
+    $chars = '';
     $password = '';
-    $max = strlen($caratteri) - 1;
+
+if($alphabetic == "on"){
+    $chars.= $alphabet;
+}
+
+if($numbers == "on"){
+    $chars.= $allNumbers;
+}
+
+if($symbols == "on"){
+    $chars.= $allSymbols;
+}
 
     for ($i = 0; $i < $length; $i++) {
-        $password .= $caratteri[random_int(0, $max)];
+        $password .= $chars[random_int(0, strlen($chars) - 1)];
     }
 
-    var_dump($caratteri);
+var_dump($alphabetic);
+var_dump($numbers);
+var_dump($symbols);
+var_dump($chars);
 
     return $password;
 }
@@ -36,7 +36,7 @@ function getSession(){
 
     if (isset($_GET['passwordLength'])) {
         $_SESSION['password'] = randomPasswordGenerator($_GET['passwordLength'], $_GET['alphabetic'], $_GET['numbers'], $_GET['symbols']);
-      }
+    }
 
 }
 
